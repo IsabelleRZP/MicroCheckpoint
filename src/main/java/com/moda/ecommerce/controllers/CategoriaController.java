2f632d42026ae6dbe1c6215bc24f09af2f5ad435
IsabelleRZP <99228352+IsabelleRZP@users.noreply.github.com>
2023-03-29 23:10:52 -0300

    Versionamento do Projeto

---
+++ src/main/java/com/moda/ecommerce/controllers/CategoriaController.java
@@ -0,0 +1,36 @@
+package com.moda.ecommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.moda.ecommerce.models.Categoria;
+import com.moda.ecommerce.models.Cliente;
+import com.moda.ecommerce.repositories.CategoriaRepository;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@GetMapping("")
+	public ModelAndView getTodosCategoriasEAbrePaginaDeCategoria() {
+		ModelAndView model = new ModelAndView("create/createCategoria");
+		List<Categoria> listaCategoria = categoriaRepository.findAll();
+		model.addObject("listaCategoria", listaCategoria);
+		return model;
+	}
+	
+	@PostMapping("/createCategoria")
+	public String create(Categoria categoria) {
+		categoriaRepository.save(categoria);
+		return "redirect:/";
+	}
+}
---
+++ src/main/java/com/moda/ecommerce/controllers/FornecedorController.java
@@ -0,0 +1,37 @@
+package com.moda.ecommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.moda.ecommerce.models.Fornecedor;
+import com.moda.ecommerce.models.Produto;
+import com.moda.ecommerce.repositories.FornecedorRepository;
+
+@Controller
+@RequestMapping("/fornecedor")
+public class FornecedorController {
+
+	@Autowired
+	private FornecedorRepository fornecedorRepository;
+	
+	
+	@GetMapping("")
+	public ModelAndView getTodosFornecedoresEAbrePaginaDeFornecedor() {
+		ModelAndView model = new ModelAndView("create/createFornecedor");
+		List<Fornecedor> listaFornecedor = fornecedorRepository.findAll();
+		model.addObject("listaFornecedor", listaFornecedor);
+		return model;
+	}
+	
+	@PostMapping("/createFornecedor")
+	public String create(Fornecedor fornecedor) {
+		fornecedorRepository.save(fornecedor);
+		return "redirect:/";
+	}
+}
